2efc9e6a2b169c6b89b7dfaf5ff9ae4f6274d844
Chris Bojemski <varcap.cb@gmail.com>
2018-01-15 14:06:13 -0500

    Making the chip logic tighter, use a list of inputs.

--- chips.py
+++ chips.py
@@ -1,39 +1,44 @@
 class Ground():
 	def __init__(self):
-		self.output = [0]
+		self.output = 0
 
 class Vcc():
 	def __init__(self):
-		self.output = [1]
+		self.output = 1
 
 class LogicChip():
-	def __init__(self, enter=[0], output=0):
+	def __init__(self, enter):
 		self.enter = enter
-		self.output = output
+		self.output = None
 
 class And(LogicChip):
-	def __init__(self, enter, output):
-		super().__init__(enter, output)
+	def __init__(self, enter):
+		super().__init__(enter)
+		self.input_values = set()
+		for i in enter:
+			self.input_values.add(i.output)
 
 	def do(self):
-		if self.enter[0] == 1 and self.enter[1] == 1:
+		if len(self.input_values) == 1:
 			self.output = 1
-		else:
+		else: 
 			self.output = 0
 
 class Or(LogicChip):
-	def __init__(self, enter, output):
-		super().__init__(enter, output)
-
+	def __init__(self, enter):
+		super().__init__(enter)
+		
 	def do(self):
-		if self.enter[0] == 1 or self.enter[1] == 1:
-			self.output = 1
-		else:
+		for i in self.enter:
+			if i.output == 1:
+				self.output = 1
+			
+		if self.output != 1:
 			self.output = 0
 
 class Xor(LogicChip):
-	def __init__(self, enter, output):
-		super().__init__(enter, output)
+	def __init__(self, enter):
+		super().__init__(enter)
 
 	def do(self):
 		if self.enter[0] == 1 and self.enter[1] == 0:
@@ -44,7 +49,7 @@ class Xor(LogicChip):
 			self.output = 0
 
 class Nand(LogicChip):
-	def __init__(self, enter, output):
+	def __init__(self, enter):
 		super().__init__(enter, output)
 
 	def do(self):
